700894b801a41664ef2c24a5ae1bed4b6f58a739
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-10-08 12:27:29 +0530

    create mongo db and minor changes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/review_app",{ useNewUrlParser: true }); // connecting to mongo database
 let app = express();
 app.set("view engine","ejs");
 app.use(express.static("public"));
@@ -8,4 +10,12 @@ app.get("/",function(req,res)
 {
   console.log("Accessed the home page");
   res.render("index");
+});
+app.get("/reviews",function(req,res)
+{
+  console.log("Accessed the reviews page");
+  res.render("reviews");
+});
+app.listen(process.env.PORT || 3000 , function(){
+  console.log("Server Started on port 3000");
 });
